a62aa66bcda877027363cd43eaba7834d661df0b
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-10-02 10:48:03 -1000

    huh

--- multiples.js
+++ multiples.js
@@ -3,10 +3,12 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
+exports.sumOfAMultiple = function (n) {
   var sum = 0;
-
-  // do your work here
-
+  for (var i = 0; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
   return sum;
 };
